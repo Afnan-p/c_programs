f091a34bc3fc103003abbb413a9549348a8ed599
Afnan-p <Afnanalanvar@gmail.com>
2025-05-21 09:07:42 +0530

    Add files via upload

--- cprograms/main.c
+++ cprograms/main.c
@@ -1,25 +1,32 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-int i,j,size=5,a;
-for(i=1;i<=size;i++){
-    for(j=1;j<=size-i;j++){
-        printf(" ");
-  
-    }
-    for(int k=1;k<=i;k++){
-        printf(" *");
-    }
-    printf("\n");
-}
+    int roll_no,phy,chemistry,computer_application;
+    char name;
+    int total;
+    float percentage;
+     printf("Input the Name of the Student:\n");
+    scanf("%s",& name);
+    
+    printf("Roll number of student:\n");
+    scanf("%d",& roll_no);
+     printf("Input the marks of Physics, Chemistry and Computer Application:\n");
+    scanf("%d%d%d",&phy,&chemistry,&computer_application);
+    total=phy+chemistry+computer_application;
+    percentage=total/3;
+    printf("total : %d",total);
+    printf("percentage of total:%f",percentage);
+    
+    
+
     return 0;
 }
