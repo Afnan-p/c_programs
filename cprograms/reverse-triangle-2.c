7dffdbd45faff87de3f5f6491e063a09aec6703b
Afnan-p <Afnanalanvar@gmail.com>
2025-05-22 21:44:42 +0530

    Add files via upload

---
+++ cprograms/reverse-triangle-2.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+int i,j,row=5;
+for(i=row; i>=1;i--){
+    for(j=1;j<=i;j++){
+     printf("* ");
+    }
+    printf("\n");
+}
+
+
+
+    return 0;
+}
