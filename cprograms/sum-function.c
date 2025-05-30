4d8b36790490ff7a89f49e0fa591fc894ee0a26e
Afnan-p <Afnanalanvar@gmail.com>
2025-05-30 11:55:34 +0530

    Add files via upload

---
+++ cprograms/sum-function.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+void sum(int number1,int number2){
+        int sum;
+        sum=number1+number2;
+            printf("sum is: %d",sum);
+        
+    }
+ int main()
+{
+sum(2,4);
+    return 0;
+}
